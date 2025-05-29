eb56f17adea2ee3fb6349baa03d38a5debb11266
Yoel Augustan <yoelagustan@gmail.com>
2025-05-29 23:43:44 +0700

    Create Main Page

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container: (
+        
+    )
+
+})
