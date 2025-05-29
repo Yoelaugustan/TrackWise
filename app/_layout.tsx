243a4b308041a1ad0966bb4369a3d99f28ae91df
Yoel Augustan <yoelagustan@gmail.com>
2025-05-30 01:33:18 +0700

    Welcome Page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { HeaderShownContext } from '@react-navigation/elements'
 
 const _layout = () => {
   return (
-    <Stack></Stack>
+    <Stack screenOptions={{ headerShown: false }}></Stack>
   )
 }
 
